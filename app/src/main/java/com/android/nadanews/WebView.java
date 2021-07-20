6a83510c7822e34b3b068c42811ffe044585abd8
nisrinaqa28 <nisrina.qurratuain.tif19@polban.ac.id>
2021-07-20 18:13:33 +0700

    pertama

---
+++ app/src/main/java/com/android/nadanews/WebView.java
@@ -0,0 +1,35 @@
+package com.android.nadanews;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.webkit.WebViewClient;
+
+public class WebView extends AppCompatActivity {
+
+    private String mUrl;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.webview);
+
+        Intent intent = getIntent();
+        mUrl = intent.getStringExtra("url");
+
+        initWebView(mUrl);
+    }
+
+    private void initWebView(String url){
+        android.webkit.WebView webView = findViewById(R.id.webView);
+        webView.getSettings().setLoadsImagesAutomatically(true);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setDomStorageEnabled(true);
+        webView.getSettings().setSupportZoom(true);
+        webView.getSettings().setBuiltInZoomControls(true);
+        webView.getSettings().setDisplayZoomControls(false);
+        webView.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl(url);
+    }
+}
